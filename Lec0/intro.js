268692abad95be909cd0b7b7858dcf8cba0dbfe1
Aditya <beingaditya2014@gmail.com>
2021-05-17 01:00:13 +0530

    folder sorting

--- Lec0/intro.js
+++ Lec0/intro.js
@@ -1,4 +1,55 @@
-console.log("su");
-console.log("suru");
-console.log("su");
-console.log("suru");
+let fs=require("fs");
+let extensions=require("./names.js");
+let path = require("path");
+
+let folderpath="./folder";
+let extensionpath;
+
+
+function checkfolder(extension){
+   for(let key in extensions){
+       if(extensions[key].includes(extension)){
+        extensionpath=`${folderpath}/${key}`;
+        break;
+       }
+   }
+   return fs.existsSync(extensionpath);
+}
+
+function movefile(filename){
+    let currentpath=`${folderpath}/${filename}`;
+    let finalpath=`${extensionpath}/${filename}`;
+    fs.copyFileSync(currentpath,finalpath);
+    fs.unlinkSync(currentpath);
+
+}
+
+function makefolder(){
+    fs.mkdirSync(extensionpath);
+}
+
+function foldersort(folderpath){
+    let files=fs.readdirSync(folderpath);
+    for(let i=0;i<files.length;i++){
+        let extension=path.extname(files[i]);
+        let exist=checkfolder(extension);
+        if(exist){
+            console.log("exist");
+            movefile(files[i]);
+        }else{
+            console.log("not exist");
+            makefolder();
+            movefile(files[i]);
+        }
+
+    }
+}
+
+foldersort(folderpath);
+
+
+
+
+
+
+
